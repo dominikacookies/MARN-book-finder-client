e1f9c09a9e546d8e92c97bb8496212cbfc2fd2f9
Dominika <domi_nika@hotmail.co.uk>
2021-08-04 21:02:52 +0100

    Updates

---
+++ precache-manifest.b5f4c179e1f2dec620a9f78a0f117dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad0e045d0492e79af8a71cf6818691b",
+    "url": "/MARN-book-finder-client/index.html"
+  },
+  {
+    "revision": "b1b817c8dfa5a1393fd8",
+    "url": "/MARN-book-finder-client/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "8a16ac84c474dbcbfdc5",
+    "url": "/MARN-book-finder-client/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "b1b817c8dfa5a1393fd8",
+    "url": "/MARN-book-finder-client/static/js/2.3a20ec1c.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/MARN-book-finder-client/static/js/2.3a20ec1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a16ac84c474dbcbfdc5",
+    "url": "/MARN-book-finder-client/static/js/main.1979998e.chunk.js"
+  },
+  {
+    "revision": "8abec90d571083d30578",
+    "url": "/MARN-book-finder-client/static/js/runtime-main.c2e55d65.js"
+  }
+]);
